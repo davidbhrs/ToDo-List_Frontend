0333943ab0ad9cb187556700615b36b56de657f7
David Bährens <d.baehrens@web.de>
2020-06-25 10:42:59 +0200

    changes

--- src/app/datastorage.service.ts
+++ src/app/datastorage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { threadId } from 'worker_threads';
+//import { threadId } from 'worker_threads';  ?? Woher kommt das ??
 
 @Injectable({
   providedIn: 'root'
