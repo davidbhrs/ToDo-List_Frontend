d0ddb65f691bc8dcadc76ff016d87568e6f45d7c
davidbhrs <d.baehrens@web.de>
2020-07-03 11:19:22 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {TextFieldModule} from '@angular/cdk/text-field';
+import {MatTableModule} from '@angular/material/table';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -50,7 +51,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatDividerModule,
     MatIconModule,
     MatListModule,
-    TextFieldModule
+    TextFieldModule,
+    MatTableModule
   ],
   providers: [DatastorageService, MatDatepickerModule],
   bootstrap: [AppComponent]
